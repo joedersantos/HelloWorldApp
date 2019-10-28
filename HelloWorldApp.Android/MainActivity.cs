2d73b110fa0edceb7cbc1ba7367a4f6ee7760077
Joéder Marques dos Santos <joeder.santos@accelera.com.br>
2019-10-28 14:26:13 -0300

    Add project files.

---
+++ HelloWorldApp.Android/MainActivity.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Microsoft.AppCenter.Push;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+
+namespace HelloWorldApp.Droid
+{
+    [Activity(Label = "HelloWorldApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+
+            Push.PushNotificationReceived += (sender, e) =>
+            {
+                Toast.MakeText(this, e.Message,
+                           ToastLength.Long).Show();
+
+                Analytics.TrackEvent("Recebeu a notificação!");
+
+                System.Diagnostics.Debug.WriteLine(e.Message);
+            };
+
+            AppCenter.Start("f3d2e951-3692-4703-94b8-91ba96a0e461"
+                            , typeof(Analytics)
+                            , typeof(Push)
+                           );
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        protected override void OnNewIntent(Android.Content.Intent intent)
+        {
+            base.OnNewIntent(intent);
+            Push.CheckLaunchedFromNotification(this, intent);
+        }
+    }
+}
